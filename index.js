738a680fc7d50511073a1c6dd8ee6ce906030b8a
Vigneswara28 <koppulavignesh123@gmail.com>
2026-05-07 15:18:53 +0530

    Updated calculator

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const add = () => {
-    event.preventDefault()
-    let val1 = Number(document.getElementById("value1").value)
-    let val2 = Number(document.getElementById("value2").value)
-    let display = document.getElementById("display")
-    display.innerHTML = `The sum of ${val1} and ${val2} is ${val1 + val2}`
+let display = document.getElementById("display")
+
+function appendValue(value){
+
+    display.value += value
 }
-const sub = () => {
-    event.preventDefault()
-    let val1 = Number(document.getElementById("value1").value)
-    let val2 = Number(document.getElementById("value2").value)
-    let display = document.getElementById("display")
-    display.innerHTML = `The substract of ${val1} and ${val2} is ${val1 - val2}`
+
+function clearDisplay(){
+
+    display.value = ""
 }
-const mul = () => {
-    event.preventDefault()
-    let val1 = Number(document.getElementById("value1").value)
-    let val2 = Number(document.getElementById("value2").value)
-    let display = document.getElementById("display")
-    display.innerHTML = `The Multiplication of ${val1} and ${val2} is ${val1 * val2}`
+
+function deleteLast(){
+
+    display.value = display.value.slice(0,-1)
 }
-const div = () => {
-    event.preventDefault()
-    let val1 = Number(document.getElementById("value1").value)
-    let val2 = Number(document.getElementById("value2").value)
-    let display = document.getElementById("display")
-    display.innerHTML = `The Division of ${val1} and ${val2} is ${val1 / val2}`
+
+function calculate(){
+
+    try{
+
+        display.value = eval(display.value)
+    }
+
+    catch{
+
+        display.value = "Error"
+    }
 }
